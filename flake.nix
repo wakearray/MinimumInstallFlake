652f92130b9b71cf603518a7eeecf50efaf76785
Kent <kent.hambrock@gmail.com>
2025-08-10 18:12:04 -0400

    Added nixinate

--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Declarative disk partitioning
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Nvim configs handled with Nixlang/Flakes
     nixvim = {
@@ -11,15 +15,21 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
+
+    # Automated deployment scripts
+    nixinate.url = "github:matthewcroughan/nixinate";
   };
 
   outputs = {
+    self,
     nixpkgs,
     disko,
     nixvim,
+    nixinate,
     ...
   }:
   {
+    apps = nixinate.nixinate.x86_64-linux self;
     nixosConfigurations.Hamburger = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -27,6 +37,15 @@
         #disko.nixosModules.disko
         ./hosts/hamburger/configuration.nix
         ./modules
+        {
+          _module.args.nixinate = {
+            host = "5.161.77.151";
+            sshUser = "nixos";
+            buildOn = "local"; # valid args are "local" or "remote"
+            substituteOnTarget = true; # if buildOn is "local" then it will substitute on the target, "-s"
+            hermetic = true;
+          };
+        }
       ];
     };
     # Use this for all other targets
